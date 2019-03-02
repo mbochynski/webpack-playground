4d5bde96221205ad1e978c75ba61fbb9777662fb
MBochynski <matbochynski@gmail.com>
2019-03-02 21:41:54 +0100

    Add file loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import MyImage from './image.png';
 import './style.css';
 
 function component() {
@@ -6,6 +7,12 @@ function component() {
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  var myIcon = new Image();
+  myIcon.src = MyImage;
+
+  element.appendChild(myIcon);
 
   return element;
 }
