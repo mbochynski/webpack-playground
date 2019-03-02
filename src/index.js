e2bf8475ee977f1befb9ea8663eeaac75f8e7414
MBochynski <matbochynski@gmail.com>
2019-03-02 22:05:15 +0100

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import _ from 'lodash';
-import MyImage from './image.png';
-import './style.css';
+import _ from "lodash";
+import MyImage from "./image.png";
+import "./style.css";
+import printMe from "./print";
 
 function component() {
-  let element = document.createElement('div');
+  let element = document.createElement("div");
+  let button = document.createElement("button");
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  button.innerHTML = "Click me and check the console!";
+  button.onclick = printMe;
+
+  element.appendChild(button);
 
   // Add the image to our existing div.
   var myIcon = new Image();
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,31 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: "./src/index.js",
+    print: "./src/print.js"
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist/*"]),
+    new HtmlWebpackPlugin({
+      title: "Output Management"
+    })
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ]
+        use: ["file-loader"]
       }
     ]
   }
